b4e28974dc5be0b8de825601fa293f8a3a5a0cd0
guan-noah <nguan651@student.fuhsd.org>
2024-10-11 10:38:09 -0700

    Create 1. SummerVacation.java

---
+++ 1. SummerVacation.java
@@ -0,0 +1,35 @@
+//Noah Guan
+//8-28-24 Per 6
+/*
+SummerVacation.java
+Describes what I did over the summer
+Our second project! 
+*/
+
+/*
+Instructions: Write SummerVacation.java, which will: 
+	Print out sentences and lists about what you did this summer. 
+	Follow the format of the example given. Include a minimum of 6 lines, not counting blank lines. 
+	Use methods print(), println(), and escape sequences for tab, new line, and quotes. The 3 blank lines do not count for the escape sequence for new lines. 
+	Using print with the 3 blank lines does not count and is bad. 
+	Use print and a println statement to write 2 things on the same line in the terminal window. 
+	Do not use lots of spaces to place your words appropriately. 
+	Be a one-method (main) and one-class program. 
+	Have skipped lines and indentation to make your program easy to read.
+*/
+
+public class SummerVacation
+{
+	public static void main(String[] args)
+	{
+		System.out.println("\n\n\n");
+		System.out.println("\t\"What I Did Over Summer Vacation!\"");
+		System.out.println("\nAs soon as school ended, I flew to London to tag along for " +
+			"my dad's work trip");
+		System.out.println("After we came back from London, we had a few weeks of rest at home before we flew to Asia to visit relatives.");
+		System.out.print("We flew to Taiwan for 3 weeks, ");
+		System.out.println("but we got a bit homesick around the 2 1/2 week.");
+		System.out.println("Being on a vacation is very fun; however, it's always nice to return back home.");
+		System.out.println("\n\n\n");
+	}
+}
