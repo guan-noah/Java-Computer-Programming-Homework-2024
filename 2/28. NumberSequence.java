f44b88d3c239abd7374fe8ebe168385c591de04f
guan-noah <nguan651@student.fuhsd.org>
2024-11-20 15:29:30 -0800

    Update 28. NumberSequence.java

--- 2/28. NumberSequence.java
+++ 2/28. NumberSequence.java
@@ -28,9 +28,11 @@
 public class NumberSequence
 {
 	private int startNum, sequenceRule, numAnswer, userNumAnswer;
-		//
+		//declare field variables 
 	private Scanner keyboard;
-	public NumberSequence()
+		//field scanner! :D
+	private boolean done;
+	public NumberSequence()//initialize all field variables 
 	{
 		startNum = 0;
 		sequenceRule = 0; //this is the increment/decrement 
@@ -40,25 +42,34 @@ public class NumberSequence
 			//be careful -- if something goes wrong in reinitializing 
 			//numAnswer and userNumAnswer, it will both be the same 
 			//(interpreted as user was correct). 
+		done = false;
 	}
 	public static void main(String[] args)
 	{
 		NumberSequence ns = new NumberSequence();
+			//make a new instance of numbersequence
 		ns.newLines();
+			//call newlines (required) 
 		ns.play();
+			//call play to run the code 
 		ns.newLines();
+			//call newlines (required) 
 	}
 	public void newLines()
 	{
 		System.out.println("\n\n\n");
+			//prints the required 3 new lines 
 	}
 	public void run() //this method runs the program. 
 	{
 		prompt();
 		generateSequence();
 		printSequence();
-		getNum()
-		
+		//nextNumProblem();
+		if (!done)
+		{
+			run()
+		}
 	}
 	public void prompt()
 	{
@@ -68,11 +79,56 @@ public class NumberSequence
 			+ " in the underscore. \nYou will get at most 3 tries to guess the next"
 			+ " number; after that, you will get one try to guess the rule"
 			+ " for the sequence, whether you guessed the first number or not. ");
+			//explains the game 
 		
 	}
 	public void generateSequence()
 	{
-		startNum = (int)(Math.random()*22 - 10)
+		startNum = (int)(Math.random()*22 - 10);
 			//this is 22 because it includes -10 to 0, 0 included, 
+		sequenceChange = (int)(Math.random()*10 + 1);
+	}
+	public void printSequence()
+	{
+		System.out.println("Your sequence is:");
+		outputSequence(0);
+	}
+	public void outputSequence(int countIn)
+	{
+		if (count < 5)
+		{
+			System.out.println((sequenceRule*count+startNum) + "\t");
+		}
+		else
+			System.out.println();
+			numAnswer = sequenceRule*count+startNum;
+	}
+	public void nextNumProblem()
+	{
+		getNextNum();
+		decideIfNumCorrect();
+		printOutput();
+	}
+	public void getNextNum()
+	{
+		System.out.print("What's the next number?\t");
+		userNum = keyboard.nextInt();
+	}
+	public void decideIfNumCorrect()
+	{
+		if (userNum == numAnswer)
+		{
+			correct = true;
+		}
+		else if (sequenceRule
+	}
+	public void printOutput()
+	{
+		System.out.println("");
+	}
+	public void getRule()
+	{
+		System.out.print("How do you go from one number to the next?\t");
+		userNum = keyboard.nextInt();
 	}
 }
