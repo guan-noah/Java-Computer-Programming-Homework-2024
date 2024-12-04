ca9a276ac1b05a897c389b2979564a7f479943ac
guan-noah <nguan651@student.fuhsd.org>
2024-12-04 15:27:09 -0800

    Update 30. StringMethods.java

--- 2/30. StringMethods.java
+++ 2/30. StringMethods.java
@@ -9,28 +9,47 @@ public class StringMethods
 	public static void main(String[] args)
 	{
 		StringMethods sm = new StringMethods();
-		String str = new String("Algebra");
-		System.out.println(sm.charAt(str, 5));
-		System.out.println(sm.endsWith(str, "ra"));
+			//new instance of StringMethods to call non-static run method 
+		sm.run();
 	}
-	public char charAt(String str, int x) //user gives in the string they want
+	public void run()
+	{
+		String str = new String("Algebra");
+			//uses a basic string to compare to (class example) 
+		System.out.println(charAt(str, 5));
+			//uses the charAt method 
+		System.out.println(endsWith(str, "ra"));
+			//uses the endsWith method 
+	{
+	public char charAt(String str, int index) //user gives in the string they want
 		//and the index they want it at 
 	{
 		char returnChar = '/';
-			//test case if it fails 
-		String a = " ";
-		int b = 0;
-		String y = str.substring(x, x+1);
-		b = y.compareTo(a);
-		returnChar = (char)(b + 32);
+			//initialize to catch case if it fails 
+		int difference = 0;
+			//initialize to catch case 
+		String returnString = str.substring(index, index + 1);
+			//takes in the char at the index as a string 
+		difference = returnString.compareTo(" ");
+			//compare to the first ascii character 
+			//stores the difference 
+		returnChar = (char)(difference + 32);
+			//stores the difference to the space added to 32, typecasted to char
 		return returnChar;
+			//return the value 
 	}
 	public boolean endsWith(String fullString, String endCheck)
-		//user gives in the string they want to compare to and the string 
-		//they want to end with 
+		//user gives in the full string they want to compare to and the string 
+		//user wants to check for the end
 	{
 		String endOfString = new String("");
+			//declare & initialize end of string to an empty string 
 		endOfString = fullString.substring(fullString.length() - endCheck.length());
+			//string length from the beginning equals the whole string's 
+				//length subtracted by the end of the string's length; 
+				//then use substring on the string length from the beginning 
+				//to get the end of string 
+		//if the end of string equals the string user wants to check for 
 		if (endOfString.equals(endCheck))
 			return true;
 		else
