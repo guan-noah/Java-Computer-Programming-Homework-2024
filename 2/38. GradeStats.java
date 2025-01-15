0f8832d21ec3b87c5c2f80ba5369fe6324da2a7e
guan-noah <nguan651@student.fuhsd.org>
2025-01-15 15:33:52 -0800

    Create 38. GradeStats.java

---
+++ 2/38. GradeStats.java
@@ -0,0 +1,47 @@
+//GradeStats.java
+/* Noah Guan
+ * 01-15-2025
+ * Per. 6 Java w/ Mr. Yu
+ * GradeStats.java
+ * Program #38
+ * Pseudocode: 
+main 
+
+calculateIt //essentially the run method (diff.name)
+
+calcScoresUnder75Perc
+
+max
+
+min
+
+avg
+
+orderNums
+
+median 
+
+Sample	run.		User	input	in	bold.
+< prompt	info – make	it	clear!	>
+Type	in	the	score.		Type	“Quit”	to	end	the	program	-->		95
+Type	in	the	score.		Type	“Quit”	to	end	the	program	-->		65
+Type	in	the	score.		Type	“Quit”	to	end	the	program	-->		70
+Type	in	the	score.		Type	“Quit”	to	end	the	program	-->		78
+Type	in	the	score.		Type	“Quit”	to	end	the	program	-->		85
+Type	in	the	score.		Type	“Quit”	to	end	the	program	-->		77
+Type	in	the	score.		Type	“Quit”	to	end	the	program	-->		QuiT
+Here	is	the	data	you	entered:
+Student	1’s	score:		 95
+Student	2’s	score:		 65
+Student	3’s	score:		 70
+Student	4’s	score:		 78
+Student	5’s	score:		 85
+Student	6’s	score:		 77
+There	were	2	students	who	scored	below	75%:		student	2,	student	3.
+Number	of	scores: 6
+Minimum: 65
+Maximum: 95
+Average: 78.3
+Median: 77.5
+
+*/
