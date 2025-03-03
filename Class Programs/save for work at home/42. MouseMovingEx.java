0506dfc3d3c68f204894e9ef2389273c48b7bb4f
Noah Guan <nguan651@student.fuhsd.org>
2025-03-03 15:26:12 -0800

    Update 42. MouseMovingEx.java

--- Class Programs/save for work at home/42. MouseMovingEx.java
+++ Class Programs/save for work at home/42. MouseMovingEx.java
@@ -32,14 +32,14 @@ public class MouseMovingEx
 
 	public void runIt()
 	{
-		JFrame frame = new JFrame("Dragging an oval with a mouse");
-		frame.setSize( 600, 600);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setLocation(10, 10);
-		frame.setResizable(true);
-		MouseMovingExPanel mMovePanel = new MouseMovingExPanel();
-		frame.getContentPane().add( mMovePanel );
-		frame.setVisible(true);
+		JFrame frame = new JFrame("Dragging an oval with a mouse");		//JFrame name 
+		frame.setSize( 600, 600 );										//frame size = 600 px by 600 px
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);			//frame closes when clicked x button 
+		frame.setLocation(10, 10);										//upper left corner is at 10, 10
+		frame.setResizable(true);										//frame is resizable 
+		MouseMovingExPanel mMovePanel = new MouseMovingExPanel();		//d&i new instance of the panel (calling constructor)
+		frame.getContentPane().add( mMovePanel );						//add panel to frame 
+		frame.setVisible(true);											//set frame to visible 
 	}
 }  // end class MouseMovingEx
 
@@ -52,28 +52,31 @@ class MouseMovingExPanel extends JPanel implements MouseMotionListener, MouseLis
 	
 	public MouseMovingExPanel()
 	{
-		setBackground(Color.WHITE);
-		xloc = yloc = 100;
-		xMouse = yMouse = 0;
-		dragging = false;
+		//setBackground(Color.WHITE);										//set background color to white 
+		xloc = yloc = 100;												//initialize x location & y location to 100
+		xMouse = yMouse = 0;											//initialize x mouse & y mouse to 100
+		dragging = false;												//set to not dragging 
 		
-		addMouseMotionListener(this);
+		addMouseMotionListener(this);									//add the MouseMotionListener and the MouseListener to this object (listen for mouse movements and clicks)
 		addMouseListener(this);
 	}
 
 	public void paintComponent( Graphics g )
 	{
-		super.paintComponent(g);
-		width = this.getWidth();
-		height = this.getHeight();
+		super.paintComponent(g);										//fill color white
+		width = this.getWidth();										//call getWidth from this program and store in width
+		height = this.getHeight();										//same logic for height 
 		
 		g.setColor(Color.BLUE);
-		g.fillOval(xloc,yloc, 50,50);
+		g.fillOval(xloc,yloc, 50,50);									//draw blue circle at x location and y location
 		
-		requestFocusInWindow();
+		requestFocusInWindow();											//request focus, get ready for user input 
 	}
 	
-	/*  Comments */
+	/*  when mouse is pressed, get x position and y position of mouse (at click). 
+	 * then if x location is (greater than starting position and less than 
+	 * starting position + 50) and y location is the same logic but for y coordinates, dragging is true. 
+	 * after that, call repaint again to show moving circle where mouse is. */
 	public void mousePressed ( MouseEvent evt )
 	{
 		xMouse = evt.getX();
@@ -84,18 +87,21 @@ class MouseMovingExPanel extends JPanel implements MouseMotionListener, MouseLis
 		repaint();
 	}
 	
-	/* Comments  */
+	/* if mouse is released, the dragging is false or send signal that we're not dragging anymore */
 	public void mouseReleased ( MouseEvent evt ) 
 	{
 		// stop dragging
 		dragging = false;
 	}
-	
+	/* since this implements MouseMotionListener and MouseListener, we have to complete implementation */
 	public void mouseClicked ( MouseEvent evt ) {}
 	public void mouseEntered ( MouseEvent evt ) {}
 	public void mouseExited ( MouseEvent evt ) {}
 	
-	/* Comments  */
+	/* if user is dragging mouse, add current x coordinate - starting x 
+	 * coordinate to x location (of circle) and do the same with y coordinate; 
+	 * then reinitialize xMouse and yMouse with current coordinates of mouse. 
+	 * Finally, repaint the component again. */
 	public void mouseDragged(MouseEvent evt) 
 	{
 		if (dragging)
@@ -107,6 +113,6 @@ class MouseMovingExPanel extends JPanel implements MouseMotionListener, MouseLis
 			repaint();
 		}
 	}
-	
+	/* again, we have to complete implementation */
 	public void mouseMoved(MouseEvent evt) {}
 }  // end class MouseMovingExPanel
