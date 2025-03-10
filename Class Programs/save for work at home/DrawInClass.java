62263b51a8891b6a2725365c01fbfa6f5878a361
Noah Guan <nguan651@student.fuhsd.org>
2025-03-10 10:36:42 -0700

    Update DrawInClass.java
    works now

--- Class Programs/save for work at home/DrawInClass.java
+++ Class Programs/save for work at home/DrawInClass.java
@@ -4,21 +4,30 @@ public class DrawInClass extends JPanel
 {
 	public DrawInClass()
 	{
-		//super();
+		super();
 		setBackground(Color.PINK);
 	}
 	public void paintComponent(Graphics g)
 	{
 		super.paintComponent(g);
-		g.drawString("hello", 50, 100);
-		g.drawLine(50, 100, 100, 200);
+		//g.drawString("hello", 50, 100);
+		//g.drawLine(50, 100, 100, 200);
 	}
 	public static void main(String[] args)
 	{
+		JFrame frame = new JFrame("Masterpiece.java");
+		int widthScalable = 1920*4/5;
+		int heightScalable = (1080-50)*4/5;								//1920 x 1080 screen with a 50 px toolbar
+		frame.setSize(600, 500);										
+		frame.setLocation(widthScalable-600, heightScalable-500);		//sets it to the perfect screen edge (plus 50 to give space for toolbar)
+		//frame.setLocation(1320, 530);
+		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);		
 		DrawInClass dic = new DrawInClass();
+		frame.setContentPane(dic);
+		frame.setVisible(true);
+		
 		//dic.paintComponent();
 	}
-	
 }
 /*
 Drawing in JPanels 
