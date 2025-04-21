8b4de3a8d01988d5117ea5e3a07680275b841c4d
Noah Guan <nguan651@student.fuhsd.org>
2025-04-20 18:54:53 -0700

    Update GameModuleFiles.java
    BUG FIX!! totally works now ahhhhahaha my eyes hurt 
    i've been sick and staring at the screen all dayyyy over break

--- Extra HW Projects/GameModuleFiles.java
+++ Extra HW Projects/GameModuleFiles.java
@@ -50,6 +50,45 @@ exactly what goes into the text file
 	///by 0.25x speed, and double-clicked spacebar to essentially catch DeRuiter's "screenshots". 
 	///note to anyone else seeing this: work backwards from that timestamp to obtain original file.
 
+///note: timestamp 20:19 to 20:20 for compiled list of all computer questions (here we go again!)
+
+///here is my youtube video comment: 
+
+0:00 to 1:17 introduction/background info
+1:17 to 2:21 overview of program, text file, questions
+2:21 to 4:46 add lastNamePanel in StartPanel + testing 
+4:46 to 5:10 testing program (up to StartPanel)
+5:10 to 6:20 making sure text fields have text before moving on in StartPanel
+6:20 to 6:37 testing program (up to StartPanel), again 
+
+6:37 to 8:14 add first and last name to data class (6:46 to 6:47 has a valuable scroll of the entire rest of program)
+
+8:14 to 8:21 changes his mind about compiling and testing 
+8:21 to 12:28 set up the JRadioButtons in QuestionsPanel
+12:28 to 14:00 debug JRadioButtons + testing program (up to QuestionsPanel)
+14:00 to 15:17 add submit + next question buttons to panel
+15:17 to 15:53 testing program (up to QuestionsPanel)
+15:53 to 16:38 discussing actionPerformed method in QuestionsPanel
+16:38 to 16:51 brief discussion of resetQuestion in QuestionsPanel
+
+16:51 to 17:17 discussing constructor logic in HighScoresPanel
+17:17 to 17:44 paintComponent update logic in HighScoresPanel
+
+17:44 to 18:40 GameData field vars, constructors overview 
+18:40 to 26:13 grabQuestionFromFile method walkthrough in GameData (20:20 to 20:21 has a valuable scroll of the entire computerQuestions.txt)
+26:13 to 27:21 testing program (up to HighScoresPanel, the end!)
+27:21 to 33:50 saveToHighScores method walkthrough in GameData up to writing to highScores file (27:26 to 27:29 has a valuable scroll of the GameData methods) note: for better context, start at 27:15 when he's technically still testing the program but he explains what he's going to do next. also: 30:20 to 30:34 has a good lasting shot of the starting highScores.txt
+33:50 to 35:43 finishing method off with sending information to the file 
+
+35:43 to 36:26 debugging program (final)
+36:26 to 38:12 testing all aspects of program 
+
+usability notes: 
+ - I separated paragraphs by main class (first one is StartPanel, I made a new paragraph every time he switched to a different class)
+ - please email nguan651@student.fuhsd.org if you have any questions about this. 
+ - if you were like me and on spring break while doing this (not wanting to bother teachers/give them a hard time over break), welcome to lots and lots of bugs while typing out an ~600 line program. Good luck :) 
+
+source cited: Mr. Scott DeRuiter. :)
 
 */
 import java.awt.Graphics;
@@ -154,6 +193,7 @@ class StartPanel extends JPanel implements ActionListener				//1
 		firstNameField.setFont(myFont);
 		firstNamePanel.add(firstNameField);
 		
+																		/** 1. video adds next 3 paragraphs **/
 		JPanel lastNamePanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 20, 70));//change this to 2 lines if it doesn't work
 		lastNamePanel.setBackground(Color.LIGHT_GRAY);
 		add(lastNamePanel);
@@ -182,8 +222,11 @@ class StartPanel extends JPanel implements ActionListener				//1
 	{
 		String command = evt.getActionCommand();
 		
+																		/** 2. adds the 2 && name field requirements **/
 		if(command.equals("NEXT PANEL") && !firstNameField.getText().equals("") && !lastNameField.getText().equals(""))
 		{
+																		/** 3. initializes data **/
+			data.setName(firstNameField.getText(), lastNameField.getText());
 			listOfCards.next(primaryPanel);
 		}
 	}
@@ -219,9 +262,6 @@ class QuestionsPanel extends JPanel implements ActionListener
 		
 		questionArea = new JTextArea(data.getQuestion(), 3, 30);
 		questionArea.setFont(myFont);
-		
-		questionArea = new JTextArea(data.getQuestion(), 3, 30);
-		questionArea.setFont(myFont);									//what he just repeated the same code. 
 		questionArea.setLineWrap(true);
 		questionArea.setWrapStyleWord(true);
 		questionArea.setOpaque(false);
@@ -231,18 +271,45 @@ class QuestionsPanel extends JPanel implements ActionListener
 		
 		JPanel answers = new JPanel();									//d&i answers jpanel gridlayout in center 
 		answers.setBackground(Color.GRAY);
+		answers.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
 		answers.setLayout(new GridLayout(2, 2, 20, 20));
 		add(answers, BorderLayout.CENTER);
+		
+																		/** 4. add new JRadioButton group **/
+		group = new ButtonGroup();
+		for(int i = 0; i < answer.length; i++)
+		{
+			answer[i] = new JRadioButton("" + (char)(65 + i) + ". " + data.getAnswer(i));
+			group.add(answer[i]);
+			answer[i].setOpaque(true);
+			answer[i].setBackground(new Color(230, 230, 230));
+			answer[i].setFont(myFont);
+			answer[i].addActionListener(this);
+			answers.add(answer[i]);
+		}
 		
 		JPanel buttonPanel = new JPanel();								//d&i button holder jpanel flowlayout in south 
 		buttonPanel.setBackground(Color.WHITE);
 		buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 30));
 		add(buttonPanel, BorderLayout.SOUTH);
 		
+																		/** 5. add submit and nextQuestion button **/
+		submit = new JButton("SUBMIT");							//d&i submit button 
+		submit.setFont(myFont);
+		submit.addActionListener(this);
+		submit.setEnabled(false);
+		buttonPanel.add(submit);
+		
+		nextQuestion = new JButton("NEXT QUESTION");							//d&i nextpanel button 
+		nextQuestion.setFont(myFont);
+		nextQuestion.addActionListener(this);
+		nextQuestion.setEnabled(false);
+		buttonPanel.add(nextQuestion);
+		
 		nextPanel = new JButton("NEXT PANEL");							//d&i nextpanel button 
 		nextPanel.setFont(myFont);
 		nextPanel.addActionListener(this);
-		nextPanel.setEnabled(false);									//what's setenabled? 
+		nextPanel.setEnabled(false);									//allows next panel button to be clicked 
 		buttonPanel.add(nextPanel);
 	}
 	
@@ -255,11 +322,10 @@ class QuestionsPanel extends JPanel implements ActionListener
 		{
 			submit.setEnabled(true);									//allow user to click submit button 
 		}
-		
-		///duplicated command.equals("submit")?
-		
+				
 		if(command.equals("SUBMIT"))
 		{
+			//~ System.out.println("Submit button clicked.");
 			answer[data.getCorrectAnswer()].setBackground(Color.GREEN);	//set correct jradiobutton (in array answer) to green 
 			for(int i = 0; i < answer.length; i++)						//iterates through all jradiobuttons in answer 
 			{
@@ -283,7 +349,8 @@ class QuestionsPanel extends JPanel implements ActionListener
 			submit.setEnabled(false);
 			if(data.getQuestionCount() == 4)							//what's getquestioncount
 			{
-				nextQuestion.setEnabled(true);							//allow user to click nextquestion button 
+				nextPanel.setEnabled(true);								//allow user to click nextpanel button 
+				nextQuestion.setEnabled(false);
 			}
 			else
 			{
@@ -294,13 +361,14 @@ class QuestionsPanel extends JPanel implements ActionListener
 		{
 			resetQuestion();
 			nextPanel.setEnabled(false);
-			listOfCards.next(primaryPanel);
+			nextQuestion.setEnabled(false);
 		}
 		else if(command.equals("NEXT PANEL"))
 		{
 			data.resetAll();
 			resetQuestion();
 			nextQuestion.setEnabled(false);
+			nextPanel.setEnabled(false);
 			listOfCards.next(primaryPanel);
 		}
 	}
@@ -310,12 +378,22 @@ class QuestionsPanel extends JPanel implements ActionListener
 		//~ group.clearSelection();											//??? 
 		data.grabQuestionFromFile();
 		questionArea.setText(data.getQuestion());
+		
 		char[] letters = new char[] {'A', 'B', 'C', 'D'};
-		//~ answer[0].setText("A. " + data.getAnswer(0));					//used this as a framework. mr. deruiter did it all manually. 
-		for(int i = 0; i < letters.length; i++)							//have to use letters.length because deruiter only had 4 manual code snippets. too lazy to find out if I can use answers.length as well. 
+		//~ answer[0].setText("A. " + data.getAnswer(0));					///used this as a framework. mr. deruiter did it all manually. 
+		for(int i = 0; i < letters.length; i++)							///have to use letters.length because deruiter only had 4 manual code snippets. too lazy to find out if I can use answers.length as well. 
 		{
-			answer[i].setText(letters[i] + ". " + data.getAnswer(0));	
+			answer[i].setText(letters[i] + ". " + data.getAnswer(i));	
+			answer[i].setBackground(new Color(230, 230, 230));
+			answer[i].setEnabled(true);
 		}
+		
+		/* //what mr. deruiter did. 
+		answer[0].setText("A. " + data.getAnswer(0));
+		answer[1].setText("B. " + data.getAnswer(1));
+		answer[2].setText("C. " + data.getAnswer(2));
+		answer[3].setText("D. " + data.getAnswer(3));
+		*/
 	}
 }
 
@@ -356,10 +434,10 @@ class HighScoresPanel extends JPanel implements ActionListener
 		scoreInfo.setEditable(false);
 		leftSidePanel.add(scoreInfo);
 		
-		JPanel rightSidePanel = new JPanel();							//create center panel's center, rightSidePanel (wait a second, this isn't right)
+		JPanel rightSidePanel = new JPanel();							///create center panel's center, rightSidePanel (wait a second, this isn't right)
 		rightSidePanel.setBorder(BorderFactory.createEmptyBorder(30, 30, 30, 30));
 		rightSidePanel.setLayout(new BorderLayout());
-		centerPanel.add(rightSidePanel, BorderLayout.CENTER);
+		centerPanel.add(rightSidePanel, BorderLayout.CENTER);			///does the exact same thing as leftSidePanel. interesting choice. 
 		
 		highScoresArea = new JTextArea("" + data.getHighScores(), 10, 20);//initialize jtextarea highscoresarea
 		highScoresArea.setFont(myFont);
@@ -386,7 +464,7 @@ class HighScoresPanel extends JPanel implements ActionListener
 		buttonPanel.add(exit);
 	}
 	
-	public void paintComponent(Graphics g)
+	public void paintComponent(Graphics g)								//"every time a panel is drawn, paintComponent is called. even though wer're not drawing anything (ex. strings), we're using this to update gamedata every time the panel is drawn."
 	{
 		super.paintComponent(g);
 		scoreInfo.setText("" + data.toString());						//set scoreInfo text area to data toString output 
@@ -416,7 +494,7 @@ class GameData															//data holder class
 	private String question;
 	private String[] answerSet;
 	private int correctAnswer;
-	private boolean[] chosenQuestions;
+	private boolean[] chosenQuestions;									//ensures no repetition of questions in current game
 	private int questionCount;
 	private int correctCount, lastGameCorrectCount;
 	
@@ -439,11 +517,55 @@ class GameData															//data holder class
 		}
 		correctAnswer = -1;												//it will never get to this value unless program manually sets it to this value 
 		chosenQuestions = new boolean[30];
-		questionCount = correctCount = 0;								//this may be dangeroussss! it MAY link questionCount ref to correctCount ref. idk but it's a primitive type 
+		questionCount = 0;
+		correctCount = 0;								//this may be dangeroussss! it MAY link questionCount ref to correctCount ref. idk but it's a primitive type 
 	}
 	
-	public void grabQuestionFromFile()									//assumed implementation walkthrough later 
+																		/** 6. whole method created: opens a text file for reading and pulls a random question from computerQuestions.txt file. **/
+	public void grabQuestionFromFile()									//assumed implementation walkthrough later; whole method originally empty. 
 	{
+		Scanner inFile = null;
+		String fileName = "computerQuestions.txt";
+		File inputFile = new File(fileName);
+		try
+		{
+			inFile = new Scanner(inputFile);
+		}
+		catch(FileNotFoundException e)
+		{
+			System.err.printf("ERROR: Cannot open %s\n", fileName);
+			System.out.println(e);
+			System.exit(1);
+		}
+		
+																		//6.1. d&i question number to random number not already selected before
+		int questionNumber = (int)(Math.random() * 30);
+		while(chosenQuestions[questionNumber] == true)
+		{
+			questionNumber = (int)(Math.random() * 30);
+		}
+		chosenQuestions[questionNumber] = true;
+		questionCount++;
+		
+		int counter = 0;												//6.2. get to question line and get question 
+		while(inFile.hasNext() && counter < 6 * questionNumber)
+		{
+			String line = inFile.nextLine();
+			counter++;
+		}
+		question = inFile.nextLine();
+		//~ System.out.println("question: " + question);
+		
+		counter = 0;													//6.3. reset counter and gather the next 4 lines for multiple choice answers, then gather the correct choice (int)
+		while(inFile.hasNext() && counter < 4)
+		{
+			answerSet[counter] = inFile.nextLine();
+			counter++;
+		}
+		correctAnswer = inFile.nextInt();
+		//~ System.out.println("Correct answer: " + correctAnswer);
+		
+		inFile.close();													//don't forget to close scanner!
 	}
 	
 	public void setName(String f, String l)								//simple getter and setter methods follow 
@@ -454,7 +576,7 @@ class GameData															//data holder class
 	
 	public String getQuestion()
 	{
-		return "" + questionCount + ". " + question;
+		return questionCount + ". " + question;
 	}
 	
 	public String getAnswer(int index)
@@ -485,8 +607,8 @@ class GameData															//data holder class
 	public String toString()
 	{
 		String message = ", " + first + " " + last + 
-			", you answered " + 
-			lastGameCorrectCount + " out of 4 of the questions correctly.  ";//note: couldn't see the end in the video, but I watched the previous video so I know what it's supposed to look like. also, there is a double space between the 2 sentences. 
+			", you answered " + lastGameCorrectCount + 
+			" out of 4 of the questions correctly.  ";//note: couldn't see the end in the video, but I watched the previous video so I know what it's supposed to look like. also, there is a double space between the 2 sentences. 
 		if(lastGameCorrectCount > 2)
 		{
 			return "Congratulations" + message + "Your name will be " + 
@@ -523,7 +645,59 @@ class GameData															//data holder class
 		return result;
 	}
 	
+																		/** 7. final, whole method created: if high enough, saves user score to high scores file (inserts it if needed) **/
 	public void saveToHighScores()
 	{
+		if(lastGameCorrectCount >= 3)
+		{
+			String result = "";
+			boolean hasBeenAdded = false;
+			String fileName = "highScores.txt";
+			String userScore = first + " " + last + " " + lastGameCorrectCount + "/4\n";//to use later ( result += userScore )
+			Scanner inFile = null;
+			File inputFile = new File(fileName);
+			
+			try																//initialize input file scanner 
+			{
+				inFile = new Scanner(inputFile);
+			}
+			catch(FileNotFoundException e)
+			{
+				System.err.printf("ERROR: Cannot open %s\n", fileName);
+				System.out.println(e);
+				System.exit(1);
+				//~ no System.err.printStackTrace(); ?
+			}
+			
+			while(inFile.hasNext())											//simply cache all file info into string format
+			{
+				String line = inFile.nextLine();
+				if(!hasBeenAdded && Integer.parseInt("" + line.charAt(line.indexOf("/") - 1)) <= lastGameCorrectCount)//checks the value of the user score before / mark (denoting score correct/total questions). error-prone: if someone has an integer in first / last name, they can break the system. 
+				{
+					result += userScore;//format of highScores.txt
+					hasBeenAdded = true;
+				}
+				result += line + "\n";
+			}
+			if(!hasBeenAdded)
+			{
+				result += userScore;//format of highScores.txt
+			}
+			inFile.close();
+			
+			File ioFile = new File("highScores.txt");
+			PrintWriter outFile = null;
+			try
+			{
+				outFile = new PrintWriter(ioFile);
+			}
+			catch(IOException e)
+			{
+				e.printStackTrace();
+				System.exit(1);
+			}
+			outFile.print(result);
+			outFile.close();
+		}
 	}
 }
