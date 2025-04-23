e64c98e296eb72e3e0d1af7c5d8a9d0fa8d34a86
Noah Guan <nguan651@student.fuhsd.org>
2025-04-23 13:40:28 -0700

    Create Polybound.java

---
+++ Polybound/Polybound.java
@@ -0,0 +1,15 @@
+//first day
+public class Polybound
+{
+	public static void main(String[] args)
+	{
+		System.out.println("\n\n");
+		Polybound polyGame = new Polybound();
+		polyGame.run();
+		System.out.println("\n\n");
+	}
+	public void run()
+	{
+		
+	}
+}
