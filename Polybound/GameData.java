edaaa605d429ae32d1aaa07843d4c20a26b5d967
kkumar999 <108036514+kkumar0999@users.noreply.github.com>
2025-04-23 18:49:51 -0700

    Update GameData.java
    Accidentally overwrote stuff, restored it :P

--- Polybound/GameData.java
+++ Polybound/GameData.java
@@ -1,3 +1,4 @@
+/*
 import java.awt.Image;
 import javax.imageio.ImageIO;
 import java.io.IOException;
@@ -5,10 +6,7 @@ import java.io.File;
 
 public class GameData
 {
-	/**
-	 * This code was written by Krish.
-	 * 
-	 **/
+	//this code was made by Krish
 	public static Image loadImage(String fileName)
 	{
 		File imgFile = new File(fileName);
@@ -26,3 +24,71 @@ public class GameData
 		return toReturn;
 	}
 }
+*/
+
+@@ -1,65 +1,28 @@
+ /* Noah Guan
+  * GameData.java
+  * Project started 4/23/2025
+ */
+ import java.awt.CardLayout;
+ import javax.swing.JPanel;
+ //noah
+ ///krish
+ /* noah */
+ /** krish **/
+ public class GameData													//static class 
+ {
+ 	private static CardLayout polyCards;
+ 	private static JPanel polyPanel;
+ 	
+ 	public GameData()
+ 	{
+ 		initializePanelAndCardLayout();
+ 	}
+ 	public GameData(CardLayout polyCardsIn, JPanel polyPanelIn)
+ 	{
+ 		polyCards = polyCardsIn;
+ 		polyPanel = polyPanelIn;
+ 	}
+ 	
+ 	public static CardLayout initializePanelAndCardLayout()
+ 	{
+ 		polyCards = new CardLayout();
+ 		polyPanel = new JPanel();
+ 		polyPanel.setLayout(polyCards);
+ 		
+ 		MainMenuPanel menuPan = new MainMenuPanel();						//must feed in to access CardLayout; easier way would just to encapsulate all classes in GameModuleFiles and have fvs 
+ 		add(menuPan, "menu");
+ 		/*
+ 		GamePanel gamePan = new GamePanel();
+ 		add(gamePan, "game");
+ 		
+ 		OptionPanel optionsPan = new OptionPanel();
+ 		add(optionsPan, "options");
+ 		
+ 		StatsPanel statsPan = new StatsPanel();
+ 		add(statsPan, "stats");
+ 		
+ 		ImagesPanel imagesPan = new ImagesPanel();
+ 		add(imagesPan, "images");
+ 		
+ 		ProblemPanel problemPan = new ProblemPanel();
+ 		add(problemPan, "problem");
+ 		
+ 		ContentPanel contentPan = new ContentPanel();
+ 		add(contentPan, "content");
+ 		*/
+ 	}
+ 	
+ 	
+ 	public static CardLayout getPolyCards()
+ 	{
+ 		return polyCards;
+ 	}
+ 	
+ 	public static JPanel getPolyPanel()
+ 	{
+ 		return polyPanel;
+ 	}
+ }
