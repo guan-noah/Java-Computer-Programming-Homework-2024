64ce4ac84377abaf81aa64312455a7c52b3d9ea4
Noah Guan <nguan651@student.fuhsd.org>
2025-06-02 11:17:51 -0700

    move commenting folder

---
+++ Polybound Game Project Zip/commenting/Wait.java
@@ -0,0 +1,48 @@
+import javax.swing.Timer;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Wait extends Timer implements ActionListener
+{
+    private int timeElapsed;
+    private int waitTime;
+ 
+    ///defaults to seconds
+    public Wait(int waitTimeIn, ActionListener a)
+    {
+        super(1000, a);
+        addActionListener(this);
+
+        timeElapsed = -1;
+        waitTime = waitTimeIn;
+    }    
+
+    public Wait(int millisDelay, int waitTimeIn, ActionListener a)
+    {
+        super(millisDelay, a);
+        addActionListener(this);
+
+        timeElapsed = -1;
+        waitTime = waitTimeIn;
+
+        start();
+    }    
+
+    public boolean isComplete()
+    {
+        return timeElapsed == waitTime;
+    }
+
+    public void actionPerformed(ActionEvent evt)
+    {
+        if(isComplete())
+        {
+            stop();
+        }
+        else
+        {
+            timeElapsed++;
+        }
+    }
+}
